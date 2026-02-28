97f349756a2806f053b9bf772724e32dcc658a90
Jekjek-png <2024-200423@rtu.edu.ph>
2026-02-28 14:36:25 +0800

    test commit 5

--- test1.py
+++ test1.py
@@ -3,3 +3,5 @@ import streamlit as st
 
 st.title("Hello Bisaya" )
 st.write("This is a simple Streamlit app to demonstrate how bisaya is roy is.")
+
+st.bar_chart (data=1, x=None, y=None, x_label=None, y_label=None, color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
